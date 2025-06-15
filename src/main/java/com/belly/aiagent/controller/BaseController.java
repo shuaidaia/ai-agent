f4faa6815f9afa69f98a537bee274f97256443eb
belly.lin <belly.lin@dfox.cn>
2025-06-15 18:41:59 +0800

    初始化AI项目

---
+++ src/main/java/com/belly/aiagent/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.belly.aiagent.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Belly
+ * @version 1.1.0
+ * 日期 2025/6/15
+ * 时间 18:32
+ */
+@Controller
+public class BaseController {
+}
